5bf49cb3e9a926882c1fbe7a5f51fca6f95438db
RedBaron2202 <kirmarbud@gmail.com>
2020-11-05 02:26:56 +0100

    Commit 01
    5 Nov. 2020 / 02:27(GMT+1)
    
    Added ability to change user information messages language

--- bot.py
+++ bot.py
@@ -7,39 +7,67 @@ from asyncio import sleep
 import requests
 import json
 
+"""
+This section has required info to connect to Wrike:
+api_version,
+Id of Wrike fodler,
+Url
+And your Permanent token for autharization
+"""
 api_version = 4
 
-folder_id = "IEAD7MPLI4RR73ER"
+folder_id = <Wrike folder id>
 
 url = f"https://www.wrike.com/api/v{api_version}/folders/{folder_id}/tasks"
 
-headers = {
-    'authorization': "bearer eyJ0dCI6InAiLCJhbGciOiJIUzI1NiIsInR2IjoiMSJ9.eyJkIjoie1wiYVwiOjQxNzQzMTUsXCJpXCI6NzMwMjA3MyxcImNcIjo0NjIyNTM4LFwidVwiOjkzODA4ODQsXCJyXCI6XCJVU1wiLFwic1wiOltcIldcIixcIkZcIixcIklcIixcIlVcIixcIktcIixcIkNcIixcIkRcIixcIk1cIixcIkFcIixcIkxcIixcIlBcIl0sXCJ6XCI6W10sXCJ0XCI6MH0iLCJpYXQiOjE2MDQ1MzA2MDZ9.s0wRI2bsDYB77ntDDfgn9U97EIfQkGM6sm3vp1P-n9k",
-}
+headers = {<your token is here>}
+
+
+#setting command prefix for discord, can be anything
 
 client = commands.Bot(command_prefix="!")
 
+#messages for users, can be translated to any preffered language
+success = "Successfully reported bug!"
+error = "Unexpected error, try later!"
+formatError = "Wrong format!"
+
+
+#Loading event, when bot successfully loaded it will bring message 
+#to the console
 @client.event
 async def on_ready():
     print("Bot is ready")
 
+
+#Bug report command itself starts here
+#to use it, type in Discord chat: "!bug <name>:<description>(optional)
 @client.command(name="bug")
 async def bug(ctx, text: str):
+
+    #Try catch loop in case user will make mistake
     try:
         title, description = text.split(":")
     except Exception:
-        await ctx.send("Непраильный формат!")
+        await ctx.send(formatError)
     else:
+
+	#User arguments transformed into one dictionary
         data = {}
         data['title'] = title
         data['description'] = description
-    
+    	
+	#Post request itself
         r = requests.post(url, headers = headers, data = data)
+       
+        #In case operation was successfull(code 200)
+        #User will recieve notification in Discord
+        #Otherwise user will recieve error in Discord
         print(r.status_code)
         if r.status_code == 200:
-            await ctx.send("Баг репорт отправлен успешно!")
+            await ctx.send(success)
         else:
-            await ctx.send("Произошла ошибка! Попробуете позже!")
+            await ctx.send(error)
     
-
-client.run("NzczNjYzMjMwODc3ODkyNjE5.X6MgCA.cfOghycMBOGwXfC_m7wlqCaKKXY");
+#Here you should put your Discord bot token
+client.run("<Discord token here>");
